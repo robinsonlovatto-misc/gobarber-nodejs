6e268cdc457f01a3dc090a2874f063ad5f421464
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-07-01 19:13:40 +0100

    displying static files. Class: Iniciando back-end do App/ Upload de Imagens/servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 // executa a crição da conexão com a base de dados
 import './database';
@@ -8,6 +9,8 @@ import './database';
 const app = express();
 app.use(express.json());
 app.use(routes);
+// para acessar os arquivos pelo browser localgost:3333/files/nome_do_arquivo
+app.use('/files', express.static(uploadConfig.directory));
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello GoStack!' });
