ba41306aecda8ca8326ddf4c72db2b681a098e72
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-07-01 09:08:18 +0100

    until class Iniciando back-end do app/Upload de imagens/atualizando avatar

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import { TreeRepository } from 'typeorm';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import uploadConfig from '../config/upload';
 
@@ -32,9 +34,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
 
-    return response.json({ ok: true });
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
