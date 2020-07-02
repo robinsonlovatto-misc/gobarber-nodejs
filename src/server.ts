c271e05b72f6a97854ce914440b0266e4b69a89f
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-07-02 19:56:00 +0100

    Handling errors from routes Course Module: Iniciando back-end do app/Tratando exceções/LIdando com erros

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,41 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 // executa a crição da conexão com a base de dados
 import './database';
 
 const app = express();
 app.use(express.json());
-app.use(routes);
+
 // para acessar os arquivos pelo browser localgost:3333/files/nome_do_arquivo
 app.use('/files', express.static(uploadConfig.directory));
+
+app.use(routes);
+
+// middleware para tratamento global dos erros
+// abaixo a função next foi renomeada para _ para que o eslint a ignore (tem que ter uma rule no eslintrc)
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  // verifica se err é uma instancia da classe APpError, ou seja, um erro conhecido e ja tratado pela aplicação
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  // é um erro que a aplicação nao estava esperando, manda-se uma mensagem genérica
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello GoStack!' });
