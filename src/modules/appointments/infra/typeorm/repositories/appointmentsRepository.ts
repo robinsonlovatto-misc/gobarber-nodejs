facdf09625a555a705eea4834444d2cada55d719
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-08-27 19:13:10 +0100

    Liskov Substitution Principle

--- src/modules/appointments/infra/typeorm/repositories/appointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/appointmentsRepository.ts
@@ -1,17 +1,19 @@
 import { EntityRepository, Repository } from 'typeorm';
-import Appointment from '../infra/typeorm/entities/appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import Appointment from '../entities/appointment';
 
 @EntityRepository(Appointment) // o parametro passado nessa linha é o model Appointment
-class AppointmentRepository extends Repository<Appointment> {
+class AppointmentRepository extends Repository<Appointment>
+  implements IAppointmentsRepository {
   // o parametro passado na linha acima é o model Appointment novamente
 
-  public async findByDate(date: Date): Promise<Appointment | null> {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     // a função teve de ser alterada para assíncrona pq o método findOne retorna uma Promise
     const findAppointment = await this.findOne({
       where: date,
     });
 
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
